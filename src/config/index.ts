8bcc0ad4d6aee1b4e501daa71d6e0cf01a25bedf
Eunsung Lim <unsungvegetable@gmail.com>
2020-09-07 23:22:28 +0900

    fixed assets not copied to dist bug

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,12 @@
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-/*
 const envFound = dotenv.config();
-if (envFound.error) {
+if (process.env.NODE_ENV === 'development' && envFound.error) {
 	throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
-*/
 
 export default {
 	env: process.env.NODE_ENV,
